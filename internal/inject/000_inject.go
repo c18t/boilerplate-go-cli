c8cc0ebd01f1a220d4eebcfc7d24bb30008b5c78
ɯ̹t͡ɕʲi <xc18tx@gmail.com>
2024-07-23 00:04:15 +0900

    fix scaffdog template

--- internal/inject/000_inject.go
+++ internal/inject/000_inject.go
@@ -2,4 +2,14 @@ package inject
 
 import "github.com/samber/do/v2"
 
-var Injector = do.New()
+var Injector = AddProvider()
+
+func AddProvider() *do.RootScope {
+	var i = do.New()
+
+	// add your di code
+	// do.Provide(i, gateway.SomeGateway)
+	// do.Provide(i, repository.SomeRepository)
+
+	return i
+}
